ca2799b2e8fde9bae63890b6dd297dd13279c968
Charles Brandt <luftbuefel@users.noreply.github.com>
2018-05-19 00:38:35 -0700

    Update rickRoll.js
    changing var and let for scope

--- rickRoll.js
+++ rickRoll.js
@@ -1,4 +1,4 @@
-let links = document.querySelectorAll("a");
-for(var i = 0; i<links.length;i++){
+var links = document.querySelectorAll("a");
+for(let i = 0; i<links.length;i++){
   links[i].href="https://youtu.be/dQw4w9WgXcQ";
 }
